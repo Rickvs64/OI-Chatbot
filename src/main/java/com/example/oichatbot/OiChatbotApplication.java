e67421b8a9c6a2907afe6f9bf89a03801d6b3a79
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-10-30 13:57:42 +0100

    Add working DialogFlow connection example request

--- src/main/java/com/example/oichatbot/OiChatbotApplication.java
+++ src/main/java/com/example/oichatbot/OiChatbotApplication.java
@@ -9,5 +9,5 @@ public class OiChatbotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OiChatbotApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/oichatbot/resources/TestResource.java
+++ src/main/java/com/example/oichatbot/resources/TestResource.java
@@ -1,13 +1,22 @@
 package com.example.oichatbot.resources;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.google.api.client.util.Maps;
 import com.google.api.gax.paging.Page;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.*;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -34,6 +43,51 @@ public class TestResource {
     public Integer increment() {
         incrementTest ++;
         return incrementTest;
+    }
+
+    @RequestMapping("/chat/{message}")
+    public String chat(@PathVariable(value="message") String message) throws Exception {
+        List<String> texts = new ArrayList<String>();
+        texts.add(message);
+
+        Map<String, QueryResult> resultMap = detectIntentTexts("openinno", texts, "123456", "en-US");
+        return resultMap.toString();
+    }
+
+
+
+    public static Map<String, QueryResult> detectIntentTexts(String projectId, List<String> texts, String sessionId, String languageCode) throws Exception {
+        Map<String, QueryResult> queryResults = Maps.newHashMap();
+        // Instantiates a client
+        try (SessionsClient sessionsClient = SessionsClient.create()) {
+            // Set the session name using the sessionId (UUID) and projectID (my-project-id)
+            SessionName session = SessionName.of(projectId, sessionId);
+            System.out.println("Session Path: " + session.toString());
+
+            // Detect intents for each text input
+            for (String text : texts) {
+                // Set the text (hello) and language code (en-US) for the query
+                TextInput.Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
+
+                // Build the query with the TextInput
+                QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+
+                // Performs the detect intent request
+                DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+
+                // Display the query result
+                QueryResult queryResult = response.getQueryResult();
+
+                System.out.println("====================");
+                System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
+                System.out.format("Detected Intent: %s (confidence: %f)\n",
+                        queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
+                System.out.format("Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
+
+                queryResults.put(text, queryResult);
+            }
+        }
+        return queryResults;
     }
 
 }
