3dfd4b53997501cc2ae49e99a7edf31fce74e6fb
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-02 14:13:35 +0100

    Add checkModifier method to MessageParser

--- src/main/java/com/example/oichatbot/managers/MessageParser.java
+++ src/main/java/com/example/oichatbot/managers/MessageParser.java
@@ -41,16 +41,34 @@ public class MessageParser {
     private String formatEmotionBlock(String message) {
         Integer startIndex = message.indexOf("[");
         Integer endIndex = message.indexOf("]");
-        String emotionBlock = message.substring(startIndex, endIndex + 1);
+        String emotionBlock = message.substring(startIndex + 1, endIndex);  // Without the [ ].
         System.out.println("Detected emotion block: " + emotionBlock);
 
-
-
+        // Split the block up in its three individual segments.
+        // 0: Emotion to check for, 1: Modifier (e.g. <0.5), 2: String to display.
+        String[] segments = emotionBlock.split(";");
+        if (checkModifier(segments[0], segments[1])) {
+            // The text should be displayed.
+        }
+        else {
+            // The text should not be displayed.
+        }
         return "temp";
     }
 
     private String formatTime(String message) {
         // todo: format time from DialogFlow (e.g. to remove timezone and seconds).
         return message;
+    }
+
+    private Boolean checkModifier(String emotion, String modifier) {
+        // Early check in case the given emotion name doesn't even exist.
+        if (!PersonalityManager.getInstance().getEmotions().containsKey(emotion)) {
+            // The given emotion name doesn't exist.
+            System.out.println("Problem in MessageParser.checkModifier(): Emotion \"" + emotion + "\" doesn't exist.");
+            return false;
+        }
+
+        
     }
 }
