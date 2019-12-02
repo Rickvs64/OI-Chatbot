ae3651cd07e88cc19567077d5e29ef9aaf2fe276
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-02 13:30:06 +0100

    Add formatEmotionBlock and formatTime methods to MessageParser class

--- src/main/java/com/example/oichatbot/managers/MessageParser.java
+++ src/main/java/com/example/oichatbot/managers/MessageParser.java
@@ -23,6 +23,34 @@ public class MessageParser {
      * @return Formatted (readable) output message.
      */
     public String parseMessage(String message) {
-        // ...
+        String output = message;
+
+        // First we format special tags/blocks to determine whether they need to be shown or hidden.
+        // We repeat this step for every block we find (until none remain).
+        while (output.contains("[")) {
+            output = formatEmotionBlock(output);
+        }
+
+        // Now we check for other misc. text from DialogFlow that requires extra formatting, such as datetime.
+        output = formatTime(output);
+
+        // Finally return the fully formatted message.
+        return output;
+    }
+
+    private String formatEmotionBlock(String message) {
+        Integer startIndex = message.indexOf("[");
+        Integer endIndex = message.indexOf("]");
+        String emotionBlock = message.substring(startIndex, endIndex + 1);
+        System.out.println("Detected emotion block: " + emotionBlock);
+
+
+
+        return "temp";
+    }
+
+    private String formatTime(String message) {
+        // todo: format time from DialogFlow (e.g. to remove timezone and seconds).
+        return message;
     }
 }
