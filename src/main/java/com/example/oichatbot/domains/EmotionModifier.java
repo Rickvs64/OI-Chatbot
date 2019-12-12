fc443f687158348aac512961b0d01ab11aeae8d9
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-12 10:33:35 +0100

    Add EmotionModifier domain class

---
+++ src/main/java/com/example/oichatbot/domains/EmotionModifier.java
@@ -0,0 +1,38 @@
+package com.example.oichatbot.domains;
+
+/**
+ * Holds information about a specific term or phrase and how it affects the bot's emotions.
+ * For example, using the word "please" may increase its "patience" emotion by a small amount.
+ */
+public class EmotionModifier {
+    private String relevantEmotion;
+    private String relevantWord;
+    private Float modification;
+
+    public EmotionModifier() {
+    }
+
+    public String getRelevantEmotion() {
+        return relevantEmotion;
+    }
+
+    public void setRelevantEmotion(String relevantEmotion) {
+        this.relevantEmotion = relevantEmotion;
+    }
+
+    public String getRelevantWord() {
+        return relevantWord;
+    }
+
+    public void setRelevantWord(String relevantWord) {
+        this.relevantWord = relevantWord;
+    }
+
+    public Float getModification() {
+        return modification;
+    }
+
+    public void setModification(Float modification) {
+        this.modification = modification;
+    }
+}
