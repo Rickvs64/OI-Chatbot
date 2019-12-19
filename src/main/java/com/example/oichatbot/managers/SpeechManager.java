2a08f0178c033030826b0e5d250f8b245002f59c
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-19 13:20:37 +0100

    Poltergeist mode

--- src/main/java/com/example/oichatbot/managers/SpeechManager.java
+++ src/main/java/com/example/oichatbot/managers/SpeechManager.java
@@ -50,7 +50,6 @@ public class SpeechManager {
                     .build();
 
             // Build the voice request, select the language code ("en-US") and the ssml voice gender.
-            // ("neutral")
             VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
                     .setLanguageCode("en-US")
                     .setSsmlGender(SsmlVoiceGender.NEUTRAL)
@@ -59,6 +58,8 @@ public class SpeechManager {
             // Select the type of audio file you want returned.
             AudioConfig audioConfig = AudioConfig.newBuilder()
                     .setAudioEncoding(AudioEncoding.MP3)
+                    .setPitch(20.0d)
+                    .setSpeakingRate(0.5d)
                     .build();
 
             // Perform the text-to-speech request on the text input with the selected voice parameters and
